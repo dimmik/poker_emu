a7b9cab1ac7c8928800a3a4b44c0b09a6102f811
dmk <git@dimmik.org>
2025-06-20 06:52:48 +0200

    table output

--- PokerEmu/Program.cs
+++ PokerEmu/Program.cs
@@ -1,13 +1,15 @@
 ﻿
 using System.Collections.Concurrent;
 using System.ComponentModel;
+using System.ComponentModel.Design.Serialization;
+using System.Net;
 using System.Security.Cryptography;
 
 namespace PokerEmu {
 
     public class Program
     {
-        static string fn = @"c:\tmp\poker_values_1000_1.tsv";
+        static string fn = @"c:\tmp\poker_t_values_1000_1.tsv";
         static void print(string s)
         {
             Console.WriteLine(s);
@@ -19,6 +21,7 @@ namespace PokerEmu {
             int max = 1000;
             print("hand\twin rate");
             var vals = Enum.GetValues<Value>().ToArray();
+            var winTable = new Dictionary<(Card, Card), int>();
             for (int i1 = 0; i1 < vals.Length; i1++)
             {
                 for (int i2 = i1; i2 < vals.Length; i2++)
@@ -31,20 +34,39 @@ namespace PokerEmu {
                         // pair
                         var c1 = new Card(Suite.Spades, v1);
                         var c2 = new Card(Suite.Clubs, v2);
-                        SpecificGame(c1, c2, max);
+                        int res = SpecificGame(c1, c2, max);
+                        winTable[(c1, c2)] = res;
                     }
                     else
                     {
                         // suited
                         var c1 = new Card(Suite.Clubs, v1);
                         var c2 = new Card(Suite.Clubs, v2);
-                        SpecificGame(c1, c2, max);
+                        int res = SpecificGame(c1, c2, max);
+                        winTable[(c1, c2)] = res;
                         // off-suite
-                        c1 = new Card(Suite.Heart, v1);
+                        c1 = new Card(Suite.Spades, v1);
                         c2 = new Card(Suite.Clubs, v2);
-                        SpecificGame(c1, c2, max);
+                        res = SpecificGame(c1, c2, max);
+                        winTable[(c1, c2)] = res;
                     }
                 }
+            }
+            print("\n\nWin Table");
+            print($"o\\s\t{string.Join("\t", vals.Reverse().Select(v => v.ToS()))}");
+            for (int i1 = vals.Length - 1; i1 >= 0; i1--)
+            {
+                var str = $"{vals[i1].ToS()}";
+                for (int i2 = vals.Length - 1; i2 >= 0; i2--)
+                {
+                    (Card c1, Card c2) tuple;
+                    if (i2 < i1) tuple = (new(Suite.Clubs, vals[i2]), new(Suite.Clubs, vals[i1]));
+                    else if (i2 > i1) tuple = (new(Suite.Spades, vals[i1]), new(Suite.Clubs, vals[i2]));
+                    else tuple = (new(Suite.Spades, vals[i1]), new(Suite.Clubs, vals[i2]));
+                    int res = winTable[tuple];
+                    str += $"\t{res * 100.0/max:0.00}%";
+                }
+                print(str);
             }
 
 
@@ -52,9 +74,14 @@ namespace PokerEmu {
             //Console.WriteLine($"my w = {myW.Name} [{myW.Weight}, {myW.Description}], oppoW = {oppoW.Name} [{oppoW.Weight}, {oppoW.Description}], winner: {winner}");
         }
 
-        private static void SpecificGame(Card c1, Card c2, int max)
+        private static int SpecificGame(Card c1, Card c2, int max)
         {
-            Dictionary<int, int> results = new Dictionary<int, int>();
+            Dictionary<int, int> results = new Dictionary<int, int>()
+            {
+                { 0, 0 },
+                { 1, 0 },
+                { -1, 0 },
+            };
             Dictionary<string, int> winningCombos = new Dictionary<string, int>();
             for (int i = 0; i < max; i++)
             {
@@ -73,6 +100,7 @@ namespace PokerEmu {
             }
             var combos = string.Join("\t", winningCombos.OrderByDescending(x => x.Value).Select(x => $"{x.Key}\t{x.Value * 100.0 / results[1]:0.00}%"));
             print($"{c1.Value.ToS()}{c2.Value.ToS()}{(c1.Suite == c2.Suite ? "s" : "o")}\t{(results[1] * 100.0 / max):0.00}%\t{combos}");
+            return results[1];
         }
 
         private static void RandomGame(Dictionary<int, int> results, Dictionary<string, int> winningCombos, Card c1, Card c2, int max)
